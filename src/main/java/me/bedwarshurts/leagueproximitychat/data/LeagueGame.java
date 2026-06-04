b3d18863ef2235a5f9f5841c4fd6ea19358d77c0
Kukiez <dkukiez@gmail.com>
2026-06-04 03:52:43 +0300

    added LeagueGame CreateRoomHash

--- src/main/java/me/bedwarshurts/leagueproximitychat/data/LeagueGame.java
+++ src/main/java/me/bedwarshurts/leagueproximitychat/data/LeagueGame.java
@@ -1,6 +1,29 @@
 package me.bedwarshurts.leagueproximitychat.data;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record LeagueGame(List<LeaguePlayer> players) {
+    public String createRoomHash() throws NoSuchAlgorithmException {
+        String sortedPlayerIds = players.stream()
+                .map(LeaguePlayer::getRiotId)
+                .filter(riotId -> riotId != null && !riotId.isBlank())
+                .map(riotId -> riotId.trim().toLowerCase())
+                .sorted()
+                .collect(Collectors.joining("_"));
+
+        byte[] bytes = sortedPlayerIds.getBytes(StandardCharsets.UTF_8);
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hashedBytes = digest.digest(bytes);
+
+        var result = HexFormat.of().formatHex(hashedBytes);
+
+        System.out.println("Room hash: " + result);
+        return result;
+    }
 }
