120c79a9766c3a4b302847f472efb8f69229f79d
Fate Sealed <bedwarshurts@gmail.com>
2026-06-06 02:49:49 +0300

    slightly less strict

--- src/main/java/me/bedwarshurts/leagueproximitychat/position/ScreenPositionTracker.java
+++ src/main/java/me/bedwarshurts/leagueproximitychat/position/ScreenPositionTracker.java
@@ -560,7 +560,7 @@ public class ScreenPositionTracker {
         try {
             Imgproc.cvtColor(screen, hsv, Imgproc.COLOR_BGR2HSV);
 
-            Scalar lowerColor = this.isColorblind ? new Scalar(23, 155, 200) : new Scalar(45, 100, 100);
+            Scalar lowerColor = this.isColorblind ? new Scalar(23, 140, 200) : new Scalar(45, 100, 100);
             Scalar upperColor = this.isColorblind ? new Scalar(26, 225, 255) : new Scalar(75, 255, 255);
             Core.inRange(hsv, lowerColor, upperColor, mask);
 
